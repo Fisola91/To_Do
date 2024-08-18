c099ec97a29c1aabb1191e79b49176e095283c74
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-18 19:06:43 +0200

    implement user login/registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def border_line
+    "block border border-black rounded-md px-4 py-1 focus:shadow-outline"
+  end
+
+  def button_style_bg_blue
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:shadow-outline"
+  end
 end
