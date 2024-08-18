e8b6a550bceae9b933de76bcac6ca3e7b52b2c7f
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-18 18:56:44 +0200

    change index action to home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: :home
 
     def home
       redirect_to tasks_path if current_user
