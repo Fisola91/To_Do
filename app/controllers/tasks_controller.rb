bc4a65f9fe83feff3cac44b060f86cd7f2c77401
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-19 12:12:42 +0200

    user edit and update task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,6 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
-    p "show"
   end
 
   def create
@@ -22,9 +21,34 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id].to_i)
+    if @task.update(updated_task)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
   private
 
     def task_params
       params.require(:task).permit(:title, :description, :due_date, :status)
+    end
+
+    def status
+      task_params.fetch(:status)
+    end
+
+    def status_value
+      status == "1" ? "completed" : "doing"
+    end
+
+    def updated_task
+      task_params.merge(status: status_value)
     end
 end
