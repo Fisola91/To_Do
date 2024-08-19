35dafd6ec6e7d47d26623e5be8a8bf9fbc0e290b
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-19 08:42:22 +0200

    user can creaete and display tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
 
   validates :title , presence: true
   validates :description, presence: true
+  validates :due_date, presence: true
 
   VALID_STATUSES = ['doing', 'completed', 'incompleted']
 
