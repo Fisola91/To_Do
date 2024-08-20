4aa9c204f3e088a8f4c4500a481df7d9022b1b6a
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-20 16:26:20 +0200

    create comment model and controller files

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   # belongs_to :user
   has_many :task_assignments,  dependent: :destroy
   has_many :users, through: :task_assignments
+  has_many :comments, dependent: :destroy
 
   validates :title , presence: true
   validates :description, presence: true
