e6fb40c1160ac204fa0c5541922fd8b86e03e137
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-20 17:56:53 +0200

    route to delete a comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :tasks
+  resources :tasks do
+    resources :comments, only: %i(new create destroy)
+  end
   root 'pages#home'
 end
