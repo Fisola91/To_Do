ff7d15ddadb8bd99118a08e102fbf4d8a7a6e763
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-20 15:15:04 +0200

    user can register or login with name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # has_many :tasks
-  has_many :task_assignments
+  has_many :task_assignments, dependent: :destroy
   has_many :tasks, through: :task_assignments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
 end
