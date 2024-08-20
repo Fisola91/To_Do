228e0aa2075e5a81460e329b0327284423a2e101
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-20 17:57:29 +0200

    comment on a task and delete a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
-class CommentController < ApplicationController
+class CommentsController < ApplicationController
+  def new
+  end
+
+  def create
+    @task = Task.find(params[:task_id])
+    comment = @task.comments.new(comments_params)
+    comment.user = current_user
+    if comment.save
+      redirect_to task_path(@task)
+    end
+  end
+
+  def destroy
+    @task= Task.find(params[:task_id])
+    comment = @task.comments.find(params[:id])
+    comment.destroy
+    redirect_to task_path(@task)
+  end
+
+  private
+  def comments_params
+    params.require(:comment).permit(:content)
+  end
 end
